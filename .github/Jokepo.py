9b39e87a1f91af072d54645f119c9ff9859c286f
FelipeAlves4 <160064875+FelipeAlves4@users.noreply.github.com>
2024-05-06 20:02:05 -0300

    Create Jokepo.py

---
+++ .github/Jokepo.py
@@ -0,0 +1,46 @@
+from random import randint
+itens = ("Pedra", "Papel", "Tesoura")
+comp = randint(0, 2)
+print("""Sua opçãos 
+      [0] Pedra 
+      [1] Papel
+      [2] Tesoura""")
+jogador = int(input("Qual sua jogada? "))
+print("JO")
+print("KE")
+print("PO!!!")
+print("-="* 12)
+print(f"Computador jogou {itens[comp]}")
+print(f"Jogador jogou {itens[jogador]}")
+print("-="*12)
+
+if comp == 0:  #Computador jogou PEDRA
+    if jogador == 0: 
+        print("Empate")
+    elif jogador == 1: 
+        print("Jogador Vence")
+    elif jogador == 2:
+        print("Computador Vence")
+    else:
+        print("JOGADA INVALIDA")
+if comp == 1:   #Computador jogou PAPEL
+    if jogador == 0: 
+        print("Computador Vence")
+    elif jogador == 1: 
+        print("Empate")
+    elif jogador == 2:
+       print("Jogador Vence")
+
+    else:
+     print("JOGADA INVALIDA")
+
+if comp == 2:   #Computador jogou TESOURA
+    if jogador == 0: 
+        print("Jogador Vence")
+    elif jogador == 1: 
+        print("Computador Vence")
+    elif jogador == 2:
+        print("Empate")
+    else:
+     print("JOGADA INVALIDA")
+
